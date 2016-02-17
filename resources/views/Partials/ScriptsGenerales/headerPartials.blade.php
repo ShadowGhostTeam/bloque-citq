acddeffaa56caf4105e9cf5272c477f916dc7241
bucalexis <bucalexis@hotmail.com>
2016-02-17 16:18:08 -0600

    color de logout

--- resources/views/Partials/ScriptsGenerales/headerPartials.blade.php
+++ resources/views/Partials/ScriptsGenerales/headerPartials.blade.php
@@ -1,7 +1,7 @@
 
 <header class="header black-bg">
     <div class="sidebar-toggle-box">
-        <div style="color: #F1108C" class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+        <div style="color: #078006" class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
     </div>
 
     <!--logo start-->
