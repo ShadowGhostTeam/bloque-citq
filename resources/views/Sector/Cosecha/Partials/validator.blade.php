80f7bacdb9dbbabe4eb0b9312a72d366b000d9e8
LordOfVultures <arturovvy@live.com.mx>
2016-03-02 13:47:53 -0600

    Añadida carpeta de Views para Cosecha. Modificado el Aside de Cosecha.

---
+++ resources/views/Sector/Cosecha/Partials/validator.blade.php
@@ -0,0 +1,76 @@
+<script type="text/javascript">
+
+    $(document).ready(function() {
+
+        $('#formulario').bootstrapValidator({
+            message: 'Los valores no son válidos',
+            feedbackIcons: {
+                invalid: 'glyphicon glyphicon-remove',
+                validating: 'glyphicon glyphicon-refresh'
+            },
+            fields: {
+                sector:{
+                    validators: {
+                        notEmpty: {
+                            message: 'Seleccione una opción'
+                        }
+                    }
+                },
+
+
+                siembra:{
+                    validators: {
+                        notEmpty: {
+                            message: 'Seleccione una opción'
+                        }
+                    }
+                },
+
+
+
+                descripcion:{
+                    validators: {
+                        notEmpty: {
+                            message: 'Ingrese un número'
+                        },
+                        stringLength: {
+                            max: 3,
+                            message: 'El texto debe ser menor a 999 caracteres'
+                        },
+                    }
+                },
+                fecha:{
+                    validators: {
+                        notEmpty: {
+                            message: 'Ingrese fecha'
+                        },
+                        date: {
+                            format: 'DD/MM/YYYY',
+                            message: 'Ingrese fecha en formato dd/mm/aaaa'
+                        }
+                    }
+                }
+            }
+        });
+
+        $('#fecha')
+                .on('dp.change dp.show', function(e) {
+                    $('#formulario').data('bootstrapValidator').revalidateField('fecha');
+                });
+    });
+</script>
+
+<script type="text/javascript">
+    $(function () {
+        $('#fecha').datetimepicker({
+            format:'DD/MM/YYYY'
+
+        });
+
+    });
+
+    $('#fecha').keypress(function(event) {event.preventDefault();});
+
+
+
+</script>
---
+++ resources/views/Sector/Cosecha/consultar.blade.php
@@ -0,0 +1,87 @@
+@include('Partials.ScriptsGenerales.scriptsPartials')
+
+
+
+<section id="container" >
+    <!-- **********************************************************************************************************************************************************
+    TOP BAR CONTENT & NOTIFICATIONS
+    *********************************************************************************************************************************************************** -->
+    <!--header start-->
+    @include('Partials.ScriptsGenerales.headerPartials')
+    <!--header end-->
+
+    <!-- **********************************************************************************************************************************************************
+    MAIN SIDEBAR MENU
+    *********************************************************************************************************************************************************** -->
+    <!--sidebar start-->
+    @include('Sector.Preparacion.aside')
+    <!--sidebar end-->
+
+    <section id="container">
+        <section id="main-content">
+            <section class="wrapper site-min-height">
+                <h3><a href="{{ route('sector/preparacion') }}"><button type="button" class="btn btn-primary"><i class="glyphicon glyphicon-arrow-left"></i> Búsqueda</button></a></h3>
+                <div class="row mt">
+
+                    <!-- INICIO CONSULTAR FUNCIONES -->
+                    <div class="col-lg-12">
+                        <div class="form-panel">
+
+                            <h4 style="color:#078006"><i class="fa fa-angle-right"></i>Consultar preparación</h4><br>
+
+                            @if( isset($preparacion))
+
+
+                                <table align="right">
+                                    <tr>
+                                        <td>
+                                            <a href="{{ route('sector/preparacion/modificar/item',$preparacion->id) }}"><button class="btn btn-primary btn-xs tooltips" data-placement="top" data-original-title="Modificar"><i class="fa fa-pencil"></i></button></a>
+                                            &nbsp
+                                        </td>
+
+                                        <td>
+                                            {!! Form::open(['action'=>['preparacionSectorController@eliminar'],'role'=>'form'] )  !!}
+                                            <button class="btn btn-danger btn-xs tooltips" data-placement="top" data-original-title="Eliminar" onclick='return confirm("¿Seguro que desea eliminar la preparacion?")'><i class="fa fa-trash-o "></i></button>
+                                            <input type="hidden" name="id" value={{$preparacion->id}}>
+                                            {!! Form::close() !!}
+
+                                        </td>
+                                    </tr>
+                                </table>
+                                <br><br>
+
+                            @endif
+                            <div class="row">
+                                <br>
+                                <div class="col-md-4">
+
+                                </div>
+
+
+                                <div class="col-md-7">
+
+                                    <dl class="dl-horizontal">
+                                        <dt>Sector</dt><dd>{{ $preparacion->sector->nombre }}</dd>
+                                        <dt>Maquinaria</dt><dd>{{ $preparacion->maquinaria->nombre }}</dd>
+                                        <dt>Número de pasadas</dt><dd>{{ $preparacion->numPasadas }}</dd>
+                                        <dt>Fecha</dt><dd>{{ $preparacion->fecha }}</dd>
+                                    </dl>
+                                </div>
+
+                            </div>
+
+
+                            <br>
+
+                        </div>
+                    </div>
+                    <!-- FIN CONSULTAR FUNCIONES -->
+                </div>
+            </section>
+        </section>
+    </section>
+
+
+
+
+@include('Partials.ScriptsGenerales.scriptsPartialsAbajo')
