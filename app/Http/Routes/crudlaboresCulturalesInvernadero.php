8be42006e961523563fa8155441f5098e3e72eb6
saulzini <saulzini@gmail.com>
2016-03-08 17:30:23 -0600

    Invernadero carpeta creada en views

--- app/Http/Routes/crudlaboresCulturalesInvernadero.php
+++ app/Http/Routes/crudlaboresCulturalesInvernadero.php
@@ -1,3 +1,33 @@
 <?php
 
 
+
+/*
+ * Ajax siembra
+ * utilizando sectores
+ * */
+
+/*
+Route::get('invernadero/ajaxSiembraInvernadero/carga',function() {
+
+    $idsectores = Input::get('id');
+
+    $siembras = siembraSector::where('id_sector',$idsectores)->get();
+    $siembrasTodas=array();
+    foreach ($siembras as $siembra) {
+
+        $fechaSiembraToda=Carbon::createFromFormat('Y-m-d H:i:s', $siembra->fecha);
+
+        array_push($siembrasTodas,array(
+                'id_siembra' => $siembra->id,
+                'variedad' => $siembra->variedad,
+                'nombre' => $siembra->cultivo->nombre,
+                'fecha' => $fechaSiembraToda->format('d/m/Y'))
+
+        );
+    }
+
+
+    return Response::json($siembrasTodas);
+});
+*/
