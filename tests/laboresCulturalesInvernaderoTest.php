9bbbfb5bc3dd2649ed15b9ae5c9ff2237fea4460
saulzini <saulzini@gmail.com>
2016-03-09 19:49:33 -0600

    Modificar terminado

--- tests/laboresCulturalesInvernaderoTest.php
+++ tests/laboresCulturalesInvernaderoTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 class laboresCulturalesInvernaderoTest extends TestCase
 {
     ///////////////CREAR
+    ///para llamar a un grupo phpunit --group laboresCulturalesModificar
     /*Unidad*/
     /**
      * @group laboresCulturalesCrear
@@ -14,5 +15,22 @@ class laboresCulturalesInvernaderoTest extends TestCase
     public function testRutaConsultar(){
         $response = $this->call('GET', 'invernadero/laboresCulturales/crear');
         $this->assertEquals(200, $response->status());
+    }
+    ///////////////MODIFICAR
+    /*Unidad*/
+    /**
+     * @group laboresCulturalesModificar
+     */
+    public function testRutaModificar(){
+        $response = $this->call('GET', 'invernadero/laboresCulturales/modificar/1');
+        $this->assertEquals(200, $response->status());
+    }
+
+    /**
+     * @group laboresCulturalesModificar
+     */
+    public function testModificarIdIncorrecto(){
+        $response = $this->call('GET', 'invernadero/laboresCulturales/modificar/120');
+        $this->assertEquals(404, $response->status());
     }
 }
