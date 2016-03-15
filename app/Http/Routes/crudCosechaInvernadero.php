547c00c13abc29bac8286997a3f48a76df460e5e
LordOfVultures <arturovvy@live.com.mx>
2016-03-15 12:52:49 -0600

    Rutas y archivo de pruebas creados para CRUD Cosecha - Invernadero

--- app/Http/Routes/crudCosechaInvernadero.php
+++ app/Http/Routes/crudCosechaInvernadero.php
@@ -1,2 +1,48 @@
 <?php
 
+/*
+ * Rutas para Invernadero-Cosecha
+ */
+Route::get('invernadero/cosecha',[
+    'uses' => 'cosechaInvernaderoController@index',
+    'as' =>'invernadero/cosecha'
+
+]);
+
+
+Route::get('invernadero/cosecha/lista',[
+        'uses' =>'cosechaInvernaderoController@buscar',
+        'as' =>'invernadero/cosecha/lista']
+
+);
+
+Route::get('invernadero/cosecha/crear',[
+    'uses' => 'cosechaInvernaderoController@pagCrear',
+    'as' =>'invernadero/cosecha/crear'
+
+]);
+
+Route::post('invernadero/cosecha/crear/','cosechaInvernaderoController@crear',array('before' => 'csrf', function() {
+    //
+}));
+
+Route::get('invernadero/cosecha/modificar/{id}',[
+    'uses' => 'cosechaInvernaderoController@pagModificar',
+    'as' =>'invernadero/cosecha/modificar/item'
+
+]);
+
+
+Route::post('invernadero/cosecha/modificar','cosechaInvernaderoController@modificar',array('before' => 'csrf', function() {
+    //
+}));
+
+Route::post('invernadero/cosecha/eliminar','cosechaInvernaderoController@eliminar',array('before' => 'csrf', function() {
+
+}));
+
+Route::get('invernadero/cosecha/consultar/{id}',[
+    'uses' => 'cosechaInvernaderoController@pagConsultar',
+    'as' =>'invernadero/cosecha/consultar/item'
+
+]);
