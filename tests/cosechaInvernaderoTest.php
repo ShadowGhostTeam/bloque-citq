ffb2aa7382bc999576c5700ce2a41060306acba0
LordOfVultures <arturovvy@live.com.mx>
2016-03-15 18:48:06 -0600

    Eliminados algunos tests inservibles de Cosecha de Sector

--- tests/cosechaInvernaderoTest.php
+++ tests/cosechaInvernaderoTest.php
@@ -6,235 +6,138 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class cosechaInvernaderoTest extends TestCase
 {
+    ///////////////////////////////////////CREAR//////////////////////////////////////////////////////
 
-
-    //////////////////////////////BUSCAR//////////////////////////////////
-    //para llamar a solo un grupo phpunit --group fertilizacionBuscarSector
+    //para llamar a solo un grupo phpunit --group cosechaCrearSector
 
     /*Unidad*/
     /**
-     * @group fertilizacionBuscarSector
+     * @group cosechaCrearSector
      */
-    public function testRutaBuscar()
-    {
-        $response = $this->call('GET', 'sector/fertilizacion');
+    public function testRutaCrear(){
+        $response = $this->call('GET', 'sector/cosecha/crear');
+        $this->assertEquals(200, $response->status());
+    }
+
+    ////////////////////////////////////////////////MODIFICAR/////////////////////////////////////////////////////////////////
+
+    //para llamar a solo un grupo "phpunit --group cosechaModificarSector"
+
+    /*Unidad*/
+    /**
+     * @group cosechaModificarSector
+     */
+    public function testRutaModificar(){
+        $response = $this->call('GET', 'sector/cosecha/modificar/12');
+        $this->assertEquals(200, $response->status());
+    }
+
+    ////////////////////////////////////////////////CONSULTAR/////////////////////////////////////////////////////////
+
+//para llamar a solo un grupo "phpunit --group cosechaConsultarSector"
+
+    /*Unidad*/
+    /**
+     * @group cosechaConsultarSector
+     */
+    public function testRutaConsultar(){
+        $response = $this->call('GET', 'sector/cosecha/consultar/12');
+        $this->assertEquals(200, $response->status());
+    }
+    /**
+     * @group cosechaConsultarSector
+     */
+    public function testConsultarIdIncorrecto(){
+        $response = $this->call('GET', 'sector/cosecha/consultar/120');
+        $this->assertEquals(404, $response->status());
+    }
+
+
+    ////////////////////////////////////////////////BUSCAR///////////////////////////////////////////////////////////
+
+    //para llamar a solo un grupo "phpunit --group cosechaBuscarSector"
+
+    /*Unidad*/
+    /**
+     * @group cosechaBuscarSector
+     */
+
+    public function testRutaBuscar(){
+        $response = $this->call('GET', 'sector/cosecha');
         $this->assertEquals(200, $response->status());
     }
 
     /*Integración*/
 
     /**
-     * @group fertilizacionBuscarSector
+     * @group cosechaBuscarSector
      */
-    public function testBuscarNoParametros()
-    {
-        $this->visit('sector/fertilizacion')
+    public function testBuscarSectorCorrecto(){
+        $this->visit('sector/cosecha')
+            ->select(1,"sector")
             ->press('Buscar')
             ->see("Se encontraron");
     }
 
     /**
-     * @group fertilizacionBuscarSector
+     * @group cosechaBuscarSector
      */
-    public function testBuscarSectorCorrecto()
-    {
-        $this->visit('sector/fertilizacion')
-            ->select(1, "sector")
+    public function testBuscarFechaCorrecto(){
+        $this->visit('sector/cosecha')
+            ->type("29/02/2015","fechaInicio")
+            ->type("29/02/2016","fechaFin")
             ->press('Buscar')
             ->see("Se encontraron");
     }
 
     /**
-     * @group fertilizacionBuscarSector
+     * @group cosechaBuscarSector
      */
-    public function testBuscarFuenteCorrecto()
-    {
-        $this->visit('sector/fertilizacion')
-            ->select(1, "fuente")
+    public function testBuscarSectorFechaCorrecto(){
+        $this->visit('sector/cosecha')
+            ->type("29/02/2015","fechaInicio")
+            ->type("29/02/2016","fechaFin")
+            ->select(1,"sector")
             ->press('Buscar')
             ->see("Se encontraron");
     }
 
     /**
-     * @group fertilizacionBuscarSector
+     * @group cosechaBuscarSector
      */
-    public function testBuscarFechaCorrecto()
-    {
-        $this->visit('sector/fertilizacion')
-            ->type("29/02/2015", "fechaInicio")
-            ->type("29/02/2016", "fechaFin")
-            ->press('Buscar')
-            ->see("Se encontraron");
-    }
-
-    /**
-     * @group fertilizacionBuscarSector
-     */
-    public function testBuscarSectorFechaCorrecto()
-    {
-        $this->visit('sector/fertilizacion')
-            ->type("29/02/2015", "fechaInicio")
-            ->type("29/02/2016", "fechaFin")
-            ->select(1, "sector")
-            ->press('Buscar')
-            ->see("Se encontraron");
-    }
-
-    /**
-     * @group fertilizacionBuscarSector
-     */
-    public function testBuscarFuenteFechaCorrecto()
-    {
-        $this->visit('sector/fertilizacion')
-            ->type("29/02/2015", "fechaInicio")
-            ->type("29/02/2016", "fechaFin")
-            ->select(1, "fuente")
-            ->press('Buscar')
-            ->see("Se encontraron");
-    }
-
-    /**
-     * @group fertilizacionBuscarSector
-     */
-    public function testBuscarSectorFuenteFechaCorrecto()
-    {
-        $this->visit('sector/fertilizacion')
-            ->type("29/02/2015", "fechaInicio")
-            ->type("29/02/2016", "fechaFin")
-            ->select(1, "sector")
-            ->select(1, "fuente")
-            ->press('Buscar')
-            ->see("Se encontraron");
-    }
-
-    /**
-     * @group fertilizacionBuscarSector
-     */
-    public function testBuscarUnaFecha()
-    {
-        $this->visit('sector/fertilizacion/lista?sector=&fuente=&fechaInicio=&fechaFin=29%2F02%2F2016')
+    public function testBuscarUnaFecha(){
+        $this->visit('sector/cosecha/lista?sector=&fechaInicio=&fechaFin=29%2F02%2F2016')
             ->see("No se encontraron resultados");
     }
 
     /**
-     * @group fertilizacionBuscarSector
+     * @group cosechaBuscarSector
      */
-    public function testBuscarUnaFechaTexto()
-    {
-        $this->visit('sector/fertilizacion/lista?sector=&fuente=&fechaInicio=sdfsdfsd&fechaFin=')
+    public function testBuscarUnaFechaTexto(){
+        $this->visit('sector/cosecha/lista?sector=&fechaInicio=sdfsdfsd&fechaFin=')
+            ->see("No se encontraron resultados");
+    }
+    /**
+     * @group cosechaBuscarSector
+     */
+    public function testBuscarFechasTexto(){
+        $this->visit('sector/cosecha/lista?sector=&fechaInicio=sdfsdfsd&fechaFin=sdsdfd')
+            ->see("No se encontraron resultados");
+    }
+    /**
+     * @group cosechaBuscarSector
+     */
+    public function testBuscarSectorTexto(){
+        $this->visit('sector/cosecha/lista?sector=asdasd&fechaInicio=&fechaFin=')
+            ->see("No se encontraron resultados");
+    }
+    /**
+     * @group cosechaBuscarSector
+     */
+    public function testBuscarSectorInexistente(){
+        $this->visit('sector/cosecha/lista?sector=1000&fechaInicio=&fechaFin=')
             ->see("No se encontraron resultados");
     }
 
-    /**
-     * @group fertilizacionBuscarSector
-     */
-    public function testBuscarFechasTexto()
-    {
-        $this->visit('sector/fertilizacion/lista?sector=&fuente=&fechaInicio=sdfsdfsd&fechaFin=sdsdfd')
-            ->see("No se encontraron resultados");
-    }
-
-    /**
-     * @group fertilizacionBuscarSector
-     */
-    public function testBuscarSectorTexto()
-    {
-        $this->visit('sector/fertilizacion/lista?sector=asdasd&fuente=&fechaInicio=&fechaFin=')
-            ->see("No se encontraron resultados");
-    }
-
-    /**
-     * @group fertilizacionBuscarSector
-     */
-    public function testBuscarFuenteTexto()
-    {
-        $this->visit('sector/fertilizacion/lista?sector=&fuente=zfzfdf&fechaInicio=&fechaFin=')
-            ->see("No se encontraron resultados");
-    }
-
-    /**
-     * @group fertilizacionBuscarSector
-     */
-    public function testBuscarSectorInexistente()
-    {
-        $this->visit('sector/fertilizacion/lista?sector=1000&fuente=&fechaInicio=&fechaFin=')
-            ->see("No se encontraron resultados");
-    }
-
-    /**
-     * @group fertilizacionBuscarSector
-     */
-    public function testBuscarFuenteInexistente()
-    {
-        $this->visit('sector/fertilizacion/lista?sector=&fuente=1000&fechaInicio=&fechaFin=')
-            ->see("No se encontraron resultados");
-    }
-
-
-
-
-
-
-
-    ///////////////////////////////////////CREAR//////////////////////////////////////////////////////
-
-    //para llamar a solo un grupo phpunit --group fertilizacionCrearSector
-
-    /*Unidad*/
-    /**
-     * @group fertilizacionCrearSector
-     */
-    public function testRutaCrear()
-    {
-        $response = $this->call('GET', 'sector/fertilizacion/crear');
-        $this->assertEquals(200, $response->status());
-    }
-
-
-////////////////////////////////////////////////MODIFICAR/////////////////////////////////////////////////////////////////
-
-//para llamar a solo un grupo "phpunit --group fertilizacionModificarSector"
-
-    /*Unidad*/
-    /**
-     * @group fertilizacionModificarSector
-     */
-    public function testRutaModificar()
-    {
-        $response = $this->call('GET', 'sector/fertilizacion/modificar/12');
-        $this->assertEquals(200, $response->status());
-    }
-
-    /**
-     * @group fertilizacionModificarSector
-     */
-    public function testModificarIdIncorrecto()
-    {
-        $response = $this->call('GET', 'sector/fertilizacion/modificar/120');
-        $this->assertEquals(404, $response->status());
-    }
-
-////////////////////////////////////////////////CONSULTAR/////////////////////////////////////////////////////////
-
-//para llamar a solo un grupo "phpunit --group fertilizacionConsultarSector"
-
-    /*Unidad*/
-    /**
-     * @group fertilizacionConsultarSector
-     */
-    public function testRutaConsultar()
-    {
-        $response = $this->call('GET', 'sector/fertilizacion/consultar/12');
-        $this->assertEquals(200, $response->status());
-    }
-
-    /**
-     * @group fertilizacionConsultarSector
-     */
-    public function testConsultarIdIncorrecto()
-    {
-        $response = $this->call('GET', 'sector/fertilizacion/consultar/120');
-        $this->assertEquals(404, $response->status());
-    }
 
 }
--- tests/cosechaSectorTest.php
+++ tests/cosechaSectorTest.php
@@ -19,63 +19,6 @@ class cosechaSectorTest extends TestCase
         $this->assertEquals(200, $response->status());
     }
 
-
-    /*Integración*/
-
-    /**
-     * @group cosechaCrearSector
-     */
-
-    public function testCrearCorrecto(){
-        $this->visit('sector/cosecha/crear')
-            ->select(2,"sector")
-            ->select(1,"siembra")
-            ->type("18/02/2016","fecha")
-            ->type("Esta es la descripción","descripcion")
-            ->press('Crear')
-            ->see("La cosecha ha sido agregada");
-    }
-
-    /**
-     * @group cosechaCrearSector
-     */
-    public function testCrearNoSector(){
-        $this->visit('sector/cosecha/crear')
-            ->select(1,"siembra")
-            ->type("18/02/2016","fecha")
-            ->type("Esta es la descripción","descripcion")
-            ->press('Crear')
-            ->see("El campo sector es obligatorio");
-    }
-
-
-    /**
-     * @group cosechaCrearSector
-     */
-
-    /**
-     * @group cosechaCrearSector
-     */
-
-    public function testCrearNoFecha(){
-        $this->visit('sector/cosecha/crear')
-            ->select(1,"siembra")
-            ->type("Esta es la descripción","descripcion")
-            ->press('Crear')
-            ->see("El campo fecha es obligatorio");
-    }
-
-    /**
-     * @group cosechaCrearSector
-     */
-    public function testCrearFechaIncorrecta(){
-        $this->visit('sector/cosecha/crear')
-            ->select(1,"siembra")
-            ->type("asdas","fecha")
-            ->press('Crear')
-            ->see("fecha no corresponde al formato d/m/Y");
-    }
-
     ////////////////////////////////////////////////MODIFICAR/////////////////////////////////////////////////////////////////
 
     //para llamar a solo un grupo "phpunit --group cosechaModificarSector"
@@ -88,68 +31,6 @@ class cosechaSectorTest extends TestCase
         $response = $this->call('GET', 'sector/cosecha/modificar/12');
         $this->assertEquals(200, $response->status());
     }
-    /**
-     * @group cosechaModificarSector
-     */
-    public function testModificarIdIncorrecto(){
-        $response = $this->call('GET', 'sector/cosecha/modificar/120');
-        $this->assertEquals(404, $response->status());
-    }
-    /*Integración*/
-
-    /**
-     * @group cosechaModificarSector
-     */
-
-    public function testModificarCorrecto(){
-        $this->visit('sector/cosecha/modificar/12')
-            ->select(1,"sector")
-            ->select(1,"siembra")
-            ->type("18/02/2016","fecha")
-            ->type("Esta es la descripción.","descripcion")
-            ->press('Modificar')
-            ->see("ha sido modificada");
-    }
-    /**
-     * @group cosechaModificarSector
-     */
-
-    public function testModificarNoSector(){
-        $this->visit('sector/cosecha/modificar/12')
-            ->select("","sector")
-            ->select(1,"siembra")
-            ->type("18/02/2016","fecha")
-            ->type("Esta es la descripción.","descripcion")
-            ->press('Modificar')
-            ->see("El campo sector es obligatorio");
-    }
-
-    /**
-     * @group cosechaModificarSector
-     */
-
-    public function testModificarNoFecha(){
-        $this->visit('sector/cosecha/modificar/12')
-            ->select(1,"sector")
-            ->select(1,"siembra")
-            ->type("Esta es la descripción.","descripcion")
-            ->type("","fecha")
-            ->press('Modificar')
-            ->see("El campo fecha es obligatorio");
-    }
-
-    /**
-     * @group cosechaModificarSector
-     */
-    public function testModificarFechaIncorrecta(){
-        $this->visit('sector/cosecha/modificar/12')
-            ->select(1, "sector")
-            ->select(1,"siembra")
-            ->type("asdas","fecha")
-            ->press('Modificar')
-            ->see("fecha no corresponde al formato d/m/Y");
-    }
-
 
     ////////////////////////////////////////////////CONSULTAR/////////////////////////////////////////////////////////
 
