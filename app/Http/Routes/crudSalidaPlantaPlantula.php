f32260025220dff50d40a3ebe3134fd9eeb3afce
Pablo <lpmorales1993@gmail.com>
2016-04-11 10:38:36 -0500

    espero que ya Alexis y no se jodan reportes otra vez :)

--- app/Http/Routes/crudSalidaPlantaPlantula.php
+++ app/Http/Routes/crudSalidaPlantaPlantula.php
@@ -1,2 +1,46 @@
 <?php
 
+
+Route::get('plantula/salidaplanta',[
+    'uses' => 'salidaDePlantaController@index',
+    'as' =>'plantula/salidaplanta'
+
+]);
+
+
+Route::get('plantula/salidaplanta/lista',[
+        'uses' =>'salidaDePlantaController@buscar',
+        'as' =>'plantula/salidaplanta/lista']
+
+);
+
+Route::get('plantula/salidaplanta/crear',[
+    'uses' => 'salidaDePlantaController@pagCrear',
+    'as' =>'plantula/salidaplanta/crear'
+
+]);
+
+Route::post('plantula/salidaplanta/crear/','salidaDePlantaController@crear',array('before' => 'csrf', function() {
+    //
+}));
+
+Route::get('plantula/salidaplanta/modificar/{id}',[
+    'uses' => 'salidaDePlantaController@pagModificar',
+    'as' =>'plantula/salidaplanta/modificar/item'
+
+]);
+
+
+Route::post('plantula/salidaplanta/modificar','salidaDePlantaController@modificar',array('before' => 'csrf', function() {
+    //
+}));
+
+Route::post('plantula/salidaplanta/eliminar','salidaDePlantaController@eliminar',array('before' => 'csrf', function() {
+
+}));
+
+Route::get('plantula/salidaplanta/consultar/{id}',[
+    'uses' => 'salidaDePlantaController@pagConsultar',
+    'as' =>'plantula/salidaplanta/consultar/item'
+
+]);
