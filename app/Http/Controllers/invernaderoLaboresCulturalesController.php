7b2d4fa14359727f85fd44a0aa5ab87f6da2db50
saulzini <saulzini@gmail.com>
2016-04-18 09:49:23 -0500

    Correccion

--- app/Http/Controllers/invernaderoLaboresCulturalesController.php
+++ app/Http/Controllers/invernaderoLaboresCulturalesController.php
@@ -9,6 +9,7 @@ use App\siembraTransplanteInvernadero;
 use Carbon\Carbon;
 use App\Http\Requests;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Validator;
 
