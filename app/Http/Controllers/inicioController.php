9e327bb766b166d02b2b435b9656e6877f871196
Antonio Esper <a01202743@itesm.mx>
2016-04-19 17:09:10 -0500

    Mapa Inicio

--- app/Http/Controllers/inicioController.php
+++ app/Http/Controllers/inicioController.php
@@ -12,6 +12,8 @@ use Illuminate\Support\Facades\Hash;
 use App\maquinaria;
 use App\preparacionSector;
 use App\sector;
+use App\invernadero;
+use App\invernaderoPlantula;
 use App\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -49,9 +51,72 @@ class inicioController extends Controller
     /*
      * Devuelve el inicio con la información de los sectores, invernaderos e invernadero de plántula
      */
-    public function index() {
-        return View('Home/inicio');
+    public function index(){
+        /*
+         * Info sectores
+         */
+        $sector1 = sector::find(1);
+        $siembras_sector1 = $sector1->siembras()->where('status','Activo')->get();
+        $sector2 = sector::find(2);
+        $siembras_sector2 = $sector2->siembras()->where('status','Activo')->get();
+        $sector3 = sector::find(3);
+        $siembras_sector3 = $sector3->siembras()->where('status','Activo')->get();
+        $sector4 = sector::find(4);
+        $siembras_sector4 = $sector4->siembras()->where('status','Activo')->get();
+        $sector5 = sector::find(5);
+        $siembras_sector5 = $sector5->siembras()->where('status','Activo')->get();
+        $sector6 = sector::find(6);
+        $siembras_sector6 = $sector6->siembras()->where('status','Activo')->get();
+        $sector7 = sector::find(7);
+        $siembras_sector7 = $sector7->siembras()->where('status','Activo')->get();
+        $sector8 = sector::find(8);
+        $siembras_sector8 = $sector8->siembras()->where('status','Activo')->get();
+        $sector9 = sector::find(9);
+        $siembras_sector9 = $sector9->siembras()->where('status','Activo')->get();
+        $sector10 = sector::find(10);
+        $siembras_sector10 = $sector10->siembras()->where('status','Activo')->get();
+        $sector11 = sector::find(11);
+        $siembras_sector11 = $sector11->siembras()->where('status','Activo')->get();
+        $sector12 = sector::find(12);
+        $siembras_sector12 = $sector12->siembras()->where('status','Activo')->get();
 
+        /*
+         * Info Invernaderos
+         */
+        $invernadero1 = invernadero::find(1);
+        $siembras_invernadero1 = $invernadero1->siembras()->where('status','Activo')->get();
+        $invernadero2 = invernadero::find(2);
+        $siembras_invernadero2 = $invernadero2->siembras()->where('status','Activo')->get();
+        $invernadero3 = invernadero::find(3);
+        $siembras_invernadero3 = $invernadero3->siembras()->where('status','Activo')->get();
+        $invernadero4 = invernadero::find(4);
+        $siembras_invernadero4 = $invernadero4->siembras()->where('status','Activo')->get();
+
+        /*
+         * Info Plántula
+         */
+        $invernaderoPlantula1 = invernaderoPlantula::find(1);
+        $siembras_invernaderoPlantula1 = $invernaderoPlantula1->siembras()->where('status','Activo')->get();
+
+        return View('Home/inicio')->with([
+            'siembras_sector1' => $siembras_sector1,
+            'siembras_sector2' => $siembras_sector2,
+            'siembras_sector3' => $siembras_sector3,
+            'siembras_sector4' => $siembras_sector4,
+            'siembras_sector5' => $siembras_sector5,
+            'siembras_sector6' => $siembras_sector6,
+            'siembras_sector7' => $siembras_sector7,
+            'siembras_sector8' => $siembras_sector8,
+            'siembras_sector9' => $siembras_sector9,
+            'siembras_sector10' => $siembras_sector10,
+            'siembras_sector11' => $siembras_sector11,
+            'siembras_sector12' => $siembras_sector12,
+            'siembras_invernadero1' => $siembras_invernadero1,
+            'siembras_invernadero2' => $siembras_invernadero2,
+            'siembras_invernadero3' => $siembras_invernadero3,
+            'siembras_invernadero4' => $siembras_invernadero4,
+            'siembras_invernaderoPlantula1' => $siembras_invernaderoPlantula1
+        ]);
     }
 
 }
--- resources/views/Home/inicio.blade.php
+++ resources/views/Home/inicio.blade.php
@@ -13,31 +13,205 @@ MAIN SIDEBAR MENU
     <section id="container">
         <section id="main-content">
             <section class="wrapper site-min-height">
-                <table>
-                    <tr>
-                        <td>Invernadero 1</td>
-                        <td>Invernadero 2</td>
-                        <td>Sector 1</td>
-                        <td>Sector 2</td>
-                        <td>Sector 3</td>
-                        <td>Sector 4</td>
-                    </tr>
-                    <tr>
-                        <td>Invernadero 3</td>
-                        <td>Invernadero 4</td>
-                        <td>Sector 5</td>
-                        <td>Sector 6</td>
-                        <td>Sector 7</td>
-                        <td>Sector 8</td>
-                    </tr>
-                    <tr>
-                        <td colspan="2">Invernadero Plantula</td>
-                        <td>Sector 9</td>
-                        <td>Sector 10</td>
-                        <td>Sector 11</td>
-                        <td>Sector 12</td>
-                    </tr>
-                </table>
+                <div class="espacio"></div>
+
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-tabla">
+                    <table class="table-bordered tabla-inicio">
+                        <tr>
+                            <th colspan="4">
+                                Siembras en los Sectores
+                            </th>
+                        </tr>
+                        <tr>
+                            <td id="sector">
+                                @if($siembras_sector1 == "[]")
+                                    {{ "Sin siembras" }}
+                                @else
+                                    @foreach($siembras_sector1 as $siembra)
+                                        {{ $siembra->cultivo->nombre }} - {{ $siembra->variedad }} </br>
+                                    @endforeach
+                                @endif
+                            </td>
+                            <td id="sector">
+                                @if($siembras_sector2 == "[]")
+                                    {{ "Sin siembras" }}
+                                @else
+                                    @foreach($siembras_sector2 as $siembra)
+                                        {{ $siembra->cultivo->nombre }} - {{ $siembra->variedad }} </br>
+                                    @endforeach
+                                @endif
+                            </td>
+                            <td id="sector">
+                                @if($siembras_sector3 == "[]")
+                                    {{ "Sin siembras" }}
+                                @else
+                                    @foreach($siembras_sector3 as $siembra)
+                                        {{ $siembra->cultivo->nombre }} - {{ $siembra->variedad }} </br>
+                                    @endforeach
+                                @endif
+                            </td>
+                            <td id="sector">
+                                @if($siembras_sector4 == "[]")
+                                    {{ "Sin siembras" }}
+                                @else
+                                    @foreach($siembras_sector4 as $siembra)
+                                        {{ $siembra->cultivo->nombre }} - {{ $siembra->variedad }} </br>
+                                    @endforeach
+                                @endif
+                            </td>
+                        </tr>
+                        <tr>
+                            <td id="sector">
+                                @if($siembras_sector5 == "[]")
+                                    {{ "Sin siembras" }}
+                                @else
+                                    @foreach($siembras_sector5 as $siembra)
+                                        {{ $siembra->cultivo->nombre }} - {{ $siembra->variedad }} </br>
+                                    @endforeach
+                                @endif
+                            </td>
+                            <td id="sector">
+                                @if($siembras_sector6 == "[]")
+                                    {{ "Sin siembras" }}
+                                @else
+                                    @foreach($siembras_sector6 as $siembra)
+                                        {{ $siembra->cultivo->nombre }} - {{ $siembra->variedad }} </br>
+                                    @endforeach
+                                @endif
+                            </td>
+                            <td id="sector">
+                                @if($siembras_sector7 == "[]")
+                                    {{ "Sin siembras" }}
+                                @else
+                                    @foreach($siembras_sector7 as $siembra)
+                                        {{ $siembra->cultivo->nombre }} - {{ $siembra->variedad }} </br>
+                                    @endforeach
+                                @endif
+                            </td>
+                            <td id="sector">
+                                @if($siembras_sector8 == "[]")
+                                    {{ "Sin siembras" }}
+                                @else
+                                    @foreach($siembras_sector8 as $siembra)
+                                        {{ $siembra->cultivo->nombre }} - {{ $siembra->variedad }} </br>
+                                    @endforeach
+                                @endif
+                            </td>
+                        </tr>
+                        <tr>
+                            <td id="sector">
+                                @if($siembras_sector9 == "[]")
+                                    {{ "Sin siembras" }}
+                                @else
+                                    @foreach($siembras_sector9 as $siembra)
+                                        {{ $siembra->cultivo->nombre }} - {{ $siembra->variedad }} </br>
+                                    @endforeach
+                                @endif
+                            </td>
+                            <td id="sector">
+                                @if($siembras_sector10 == "[]")
+                                    {{ "Sin siembras" }}
+                                @else
+                                    @foreach($siembras_sector10 as $siembra)
+                                        {{ $siembra->cultivo->nombre }} - {{ $siembra->variedad }} </br>
+                                    @endforeach
+                                @endif
+                            </td>
+                            <td id="sector">
+                                @if($siembras_sector11 == "[]")
+                                    {{ "Sin siembras" }}
+                                @else
+                                    @foreach($siembras_sector11 as $siembra)
+                                        {{ $siembra->cultivo->nombre }} - {{ $siembra->variedad }} </br>
+                                    @endforeach
+                                @endif
+                            </td>
+                            <td id="sector">
+                                @if($siembras_sector12 == "[]")
+                                    {{ "Sin siembras" }}
+                                @else
+                                    @foreach($siembras_sector12 as $siembra)
+                                        {{ $siembra->cultivo->nombre }} - {{ $siembra->variedad }} </br>
+                                    @endforeach
+                                @endif
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-tabla">
+                    <table class="table-bordered tabla-inicio">
+                        <tr>
+                            <th colspan="4">
+                                Siembras en los Invernaderos
+                            </th>
+                        </tr>
+                        <tr>
+                            <td id="invernadero">
+                                @if($siembras_invernadero1 == "[]")
+                                    {{ "Sin siembras" }}
+                                @else
+                                    @foreach($siembras_invernadero1 as $siembra)
+                                        {{ $siembra->cultivo->nombre }} - {{ $siembra->variedad }} </br>
+                                    @endforeach
+                                @endif
+                            </td>
+                            <td id="invernadero">
+                                @if($siembras_invernadero2 == "[]")
+                                    {{ "Sin siembras" }}
+                                @else
+                                    @foreach($siembras_invernadero2 as $siembra)
+                                        {{ $siembra->cultivo->nombre }} - {{ $siembra->variedad }} </br>
+                                    @endforeach
+                                @endif
+                            </td>
+                        </tr>
+                        <tr>
+                            <td id="invernadero">
+                                @if($siembras_invernadero3 == "[]")
+                                    {{ "Sin siembras" }}
+                                @else
+                                    @foreach($siembras_invernadero3 as $siembra)
+                                        {{ $siembra->cultivo->nombre }} - {{ $siembra->variedad }} </br>
+                                    @endforeach
+                                @endif
+                            </td>
+                            <td id="invernadero">
+                                @if($siembras_invernadero4 == "[]")
+                                    {{ "Sin siembras" }}
+                                @else
+                                    @foreach($siembras_invernadero4 as $siembra)
+                                        {{ $siembra->cultivo->nombre }} - {{ $siembra->variedad }} </br>
+                                    @endforeach
+                                @endif
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-tabla">
+                    <table class="table-bordered tabla-inicio">
+                        <tr>
+                            <th>
+                                Siembras en el Invernadero de Plántula
+                            </th>
+                        </tr>
+                        <tr>
+                            <td id="invernadero-plantula">
+                                @if($siembras_invernaderoPlantula1 == "[]")
+                                    {{ "Sin siembras" }}
+                                @else
+                                    @foreach($siembras_invernaderoPlantula1 as $siembra)
+                                        {{ $siembra->cultivo->nombre }} - {{ $siembra->variedad }} </br>
+                                    @endforeach
+                                @endif
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+
+
+
             </section>
         </section>
     </section>
